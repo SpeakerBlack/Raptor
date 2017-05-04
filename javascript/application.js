c840c068991de3f8ae651f135954a6ede19107c6
SpeakerBlack Technologys <davidpaisa04@gmail.com>
2017-05-04 12:32:18 -0500

    Revisando y agregando funciones

--- javascript/application.js
+++ javascript/application.js
@@ -63,19 +63,6 @@ $.ajax({
 	    console.log(errorThrown);
 });
 
-//Obtener la canciones descargadas por el usuario
-$.ajax({
-		data: {id_user:-1},
-		type: "POST",
-		url:  "http://23.105.70.100/Raptor/post/user/getDownloadUser",
-	})
- .done(function( data, textStatus, jqXHR ) {
- 		console.log(jQuery.parseJSON(data));		
- })
-  .fail(function( jqXHR, textStatus, errorThrown ) {
-	    console.log(errorThrown);
-});
-
 //Obtener sugerencias
 $.ajax({
 		data: {id_user:"-1"},
@@ -84,6 +71,89 @@ $.ajax({
 	})
  .done(function( data, textStatus, jqXHR ) {
  		console.log(jQuery.parseJSON(data));		
+ })
+  .fail(function( jqXHR, textStatus, errorThrown ) {
+	    console.log(errorThrown);
+});
+
+
+// Crear una playlist
+$.ajax({
+		data: {id:"1",nombre:"test"},
+		type: "POST",
+		url:  "http://localhost:8080/Raptor/post/playlist/newPlaylist",
+	})
+ .done(function( data, textStatus, jqXHR ) {
+ 		console.log(jQuery.parseJSON(data));		
+ })
+  .fail(function( jqXHR, textStatus, errorThrown ) {
+	    console.log(errorThrown);
+});
+
+// Añadir un item a la playlist
+$.ajax({
+		data: {id:"1",id_track:"2345654",nombre_track:"test",artist_track:"nro",img_track:"http//:fertyh"},
+		type: "POST",
+		url:  "http://localhost:8080/Raptor/post/playlist/addItem",
+	})
+ .done(function( data, textStatus, jqXHR ) {
+ 		console.log((data));		
+ })
+  .fail(function( jqXHR, textStatus, errorThrown ) {
+	    console.log(errorThrown);
+});
+
+
+// Listar playlist del usuario
+$.ajax({
+		data: {id:"1"},
+		type: "POST",
+		url:  "http://localhost:8080/Raptor/post/playlist/listByUser",
+	})
+ .done(function( data, textStatus, jqXHR ) {
+ 		console.log(jQuery.parseJSON(data));		
+ })
+  .fail(function( jqXHR, textStatus, errorThrown ) {
+	    console.log(errorThrown);
+});
+
+// Listar elemento de una playlist
+
+$.ajax({
+		data: {id:"1"},
+		type: "POST",
+		url:  "http://localhost:8080/Raptor/post/playlist/listItems",
+	})
+ .done(function( data, textStatus, jqXHR ) {
+ 		console.log((data));		
+ })
+  .fail(function( jqXHR, textStatus, errorThrown ) {
+	    console.log(errorThrown);
+});
+
+// Eliminar playlist
+
+$.ajax({
+		data: {id_playlist:"2"},
+		type: "POST",
+		url:  "http://localhost:8080/Raptor/post/playlist/dropList",
+	})
+ .done(function( data, textStatus, jqXHR ) {
+ 		console.log((data));		
+ })
+  .fail(function( jqXHR, textStatus, errorThrown ) {
+	    console.log(errorThrown);
+});
+
+// Borrar item de una playlist
+
+$.ajax({
+		data: {id_playlist:"1",id_track:"2345654"},
+		type: "POST",
+		url:  "http://localhost:8080/Raptor/post/playlist/dropItem",
+	})
+ .done(function( data, textStatus, jqXHR ) {
+ 		console.log((data));		
  })
   .fail(function( jqXHR, textStatus, errorThrown ) {
 	    console.log(errorThrown);
